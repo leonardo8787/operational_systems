8be21401812081d8235a8459cdc83ab2f7811850
leonardo8787 <leocampos8787@gmail.com>
2022-09-14 08:05:54 -0300

    atualização

---
+++ 1_processos_e_treads/threads/thread3/src/mythread.c
@@ -0,0 +1,11 @@
+#include "mythread.h"
+
+void *routine(void *arg)
+{
+    ptr_thread_arg targs = (ptr_thread_arg)arg;
+
+    for (int i = 0; i < targs->value; i++)
+        printf("Thread %ld key %d value: %d \n", pthread_self(), targs->key, i);
+
+    pthread_exit(arg);
+}
